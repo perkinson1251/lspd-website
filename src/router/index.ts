f6cb93feb98d4359e360506dee1418025cdeef70
perkinson1251 <tttrashutin@gmail.com>
2024-09-05 00:18:24 +0300

    delete: faq page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import AboutView from '@/views/AboutVuew.vue'
 import CareersView from '@/views/CareersView.vue'
-import FaqView from '@/views/FaqView.vue'
 import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
@@ -17,11 +16,6 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView,
-    },
-    {
-      path: '/faq',
-      name: 'faq',
-      component: FaqView,
     },
     {
       path: '/careers',
